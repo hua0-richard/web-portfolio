b52d96c488e1ec893f561f042e4ecbcc284f4803
Richard Hua <r8hua@uwaterloo.ca>
2024-02-06 11:59:46 -0500

    feat: Inter

--- client-app/src/components/ButtonRectangle.tsx
+++ client-app/src/components/ButtonRectangle.tsx
@@ -1,7 +1,8 @@
-export default function ButtonRectangle() {
+export default function ButtonRectangle({icon, label}, {icon: any, label: string}) {
   return (
     <div className="h-[36px] border border-solid border-dark-secondary text-off-white rounded-md flex justify-center items-center">
-      test
+      <div>{icon}</div>
+      <div>{label}</div>
     </div>
   );
 }
