b2ac0d5ea906bc59570d857c436d6858cee63c00
Richard Hua <r8hua@uwaterloo.ca>
2024-02-07 15:43:23 -0500

    feat: Footer

--- client-app/src/components/Footer.tsx
+++ client-app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="w-[720px] flex justify-center items-center">
         <div className="h-[144px] flex flex-col justify-around">
         <div className="text-3xl">💻</div>
-        <div className="dark:text-off-white text-sm">Updated 2024</div>
+        <div className="dark:text-off-white text-sm">Updated Feb 2024</div>
             <div className="flex flex-between space-x-4">
                 <div className="dark: text-off-white text-sm cursor-pointer hover:underline">Build ⚙️</div>
                 <div className="dark: text-off-white text-sm cursor-pointer hover:underline">Source 📄</div>
