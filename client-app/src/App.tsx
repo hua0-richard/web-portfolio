e96f3a35cf07543e0002025a84bf1044dd4d2c75
Richard Hua <r8hua@uwaterloo.ca>
2024-02-07 12:24:24 -0500

    feat: Lightmode Bg

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,7 +7,7 @@ import Education from "./components/Education";
 
 function App() {
   return (
-    <div className="absolute top-0 start-0 w-screen h-screen bg-dark-base flex flex-col justify-start	items-center">
+    <div className="bg-white dark:bg-dark-base absolute top-0 start-0 w-screen h-screen flex flex-col justify-start items-center">
       <Menu />
       <About />
       <Projects />
