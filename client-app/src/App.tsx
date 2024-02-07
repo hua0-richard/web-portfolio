c237c68d6094a01c481c7ec73f3b81776f4a2d3d
Richard Hua <r8hua@uwaterloo.ca>
2024-02-07 15:39:39 -0500

    feat: Footer

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Experience />
       <Education />
+      <Footer />
     </div>
   );
 }
