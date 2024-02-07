dfb9040499affcb66f7ab4f8358bf424f3d22cbb
Richard Hua <r8hua@uwaterloo.ca>
2024-02-06 23:01:06 -0500

    feat: Button Styling

--- client-app/src/components/ButtonRectangle.tsx
+++ client-app/src/components/ButtonRectangle.tsx
@@ -1,8 +1,14 @@
 export default function ButtonRectangle({icon, label}, {icon: any, label: string}) {
   return (
-    <div className="h-[36px] p-[16px] border-2 border-solid border-dark-secondary bg-dark-tertiary text-off-white rounded-md flex justify-center items-center text-sm	cursor-pointer hover:border-off-white">
-      <div>{icon}</div>
-      <div>{label}</div>
+    <div className="group p-[2px] rounded-lg bg-dark-secondary cursor-pointer hover:bg-gradient-to-r from-yellow-400 to-orange-500">
+      <div className="h-[36px] p-[12px] bg-dark-tertiary rounded-md flex justify-center items-center text-md">
+        <div className="mr-[8px] text-lg">
+          {icon}
+        </div>
+        <div className="bg-off-white text-off-white group-hover:bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
+          <div>{label}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- client-app/src/components/Menu.tsx
+++ client-app/src/components/Menu.tsx
@@ -1,9 +1,18 @@
 import ButtonRectangle from "./ButtonRectangle";
+
 export default function Menu() {
   return (
-    <div className="w-[540px] flex justify-around items-center">
-      <ButtonRectangle icon="" label="Test"/>
-      <ButtonRectangle icon="" label="Test"/>
+    <div className="w-[540px] flex justify-between items-center">
+      <div className="flex">
+        <ButtonRectangle icon="😀" label="About"/>
+        <ButtonRectangle icon="🔨" label="Projects"/>
+        <ButtonRectangle icon="🧑‍💻" label="Github"/>
+      </div>
+
+      <div>
+        <ButtonRectangle icon="☀️" label=""/>
+      </div>
+
     </div>
   );
 }
