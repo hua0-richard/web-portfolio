3ea574ce2a0b5a25871864f878c5a81c9a158168
hua0-richard <r8hua@uwaterloo.ca>
2024-02-07 20:49:48 -0500

    feat: Wording

--- client-app/src/components/About.tsx
+++ client-app/src/components/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="w-[720px] flex flex-col justify-between items-start">
       <h1 className="text-6xl font-bold dark:text-white">Hey, 👋</h1>
-      <div className="my-8 dark:text-off-white">I'm a Software Developer</div>
+      <div className="my-8 dark:text-off-white text-left">I'm a Software Developer with experience in Fullstack Development. I'm currently in my final year at the University of Waterloo.</div>
     </div>
   );
 }
