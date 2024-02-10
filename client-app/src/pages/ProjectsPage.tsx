7d7729c34895dc3898de589137bc8032330be964
Richard Hua <r8hua@uwaterloo.ca>
2024-02-09 22:49:12 -0500

    feat: Fade Projects

--- client-app/src/pages/ProjectsPage.tsx
+++ client-app/src/pages/ProjectsPage.tsx
@@ -2,7 +2,10 @@ import Menu from "../components/Menu";
 export default function ProjectsPage() {
   return (
     <div className="min-h-full bg-white dark:bg-dark-base absolute top-0 start-0 w-screen h-screen flex flex-col justify-start items-center overflow-auto">
+      <div className="fade">
       <Menu />
+
+      </div>
     </div>
   );
 }
