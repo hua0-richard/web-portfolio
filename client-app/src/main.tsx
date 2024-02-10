592866a39a9c5ad367630edec9280010ff0d0826
hua0-richard <r8hua@uwaterloo.ca>
2024-02-09 22:08:32 -0500

    chore: Cleanup

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SpeedInsights/>  
       <App />
     </BrowserRouter>
   </React.StrictMode>,
