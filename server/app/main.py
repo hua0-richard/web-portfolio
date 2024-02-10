0d6272e7d39c11815342a0a81688a82dd7a80c48
hua0-richard <r8hua@uwaterloo.ca>
2024-02-10 02:01:29 -0500

    fix: Not Cold

--- server/app/main.py
+++ server/app/main.py
@@ -4,6 +4,20 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 import os
+import threading
+import time
+import requests
+
+def pingServer():
+    while True:
+        print("ping")
+        requests.get("https://web-portfolio-server-hua0-richard.vercel.app/")
+        time.sleep(300)
+
+background_thread = threading.Thread(target=pingServer)
+background_thread.daemon = True
+background_thread.start()
+
 mongoString = os.environ.get('CONN_STR')
 client = MongoClient(mongoString)
 db = client["user"]
