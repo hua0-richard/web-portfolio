695a54f5713a48f83b054dff1dbe5f39af026c91
Richard Hua <r8hua@uwaterloo.ca>
2024-02-22 13:21:29 -0500

    feat: Blog

--- client-app/src/components/Menu.tsx
+++ client-app/src/components/Menu.tsx
@@ -30,6 +30,9 @@ export default function Menu() {
         <Link to="/projects">
           <ButtonRectangle icon="🔨" label="Projects" />
         </Link>
+        <Link to="/">
+          <ButtonRectangle icon="📒" label="Blog"></ButtonRectangle>
+        </Link>
         <a href="https://github.com/hua0-richard" target="_blank">
           <ButtonRectangle icon="🧑‍💻" label="Github" />
         </a>
